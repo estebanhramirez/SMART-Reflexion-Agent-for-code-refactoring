840fad092c92b0766d9c154cb88123a3e36da8fd
hernandezr.esteban2022@gmail.com <hernandezr.esteban2022@gmail.com>
2026-06-25 00:15:36 +0200

    refactor: remove hardcoded api keys and use environment variables

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # General imports
+import os
 import re # To parse the GitHub's responses and extract modified files.
 import json # To parse the LLM's responses and extract the tool calls.
 import subprocess # To execute the GitHub commands and capture their output.
@@ -21,6 +22,7 @@ from langgraph.graph import END
 from langgraph.graph import MessageGraph # To streamline workflow creation and state management: LangGraph provides the prebuilt solution `MessageGraph`; This simplifies the process of setting up conversational workflows by handling the underlying structure automatically.
 
 # LLMs imports
+from langchain_groq import ChatGroq # To create the LLM that will drive the agent's reasoning and tool usage.
 from langchain_google_genai import ChatGoogleGenerativeAI # To create the LLM that will drive the agent's reasoning and tool usage.
 
 # Pydantic imports
@@ -60,7 +62,7 @@ def run_git_diff(modified_files):
 
 # =========================================== Tools ===========================================
 
-@tool
+
 def get_modified_files() -> List[str]:
     """
         A function that retrieves the list of modified files in a GitHub repository by executing the 'git status' command and parsing its output.
@@ -76,7 +78,7 @@ def get_modified_files() -> List[str]:
     modified_files = [match.strip() for match in matches] # Clean up any trailing whitespace from the file names
     return (modified_files)
 
-@tool
+
 def get_file_changes(modified_files: List[str]) -> str:
     """
         A function that retrieves the changes in the modified files of a GitHub repository by executing the 'git diff' command and parsing its output.
@@ -89,7 +91,7 @@ def get_file_changes(modified_files: List[str]) -> str:
     git_diff_output = run_git_diff(modified_files)
     return (git_diff_output)
 
-@tool
+
 def get_file_content(file_path: str, encoding: str = "utf-8") -> str:
     """
         Opens a file and returns its content as a string.
@@ -113,7 +115,7 @@ def get_file_content(file_path: str, encoding: str = "utf-8") -> str:
         raise UnicodeDecodeError(f"Error: Could not decode the file using {encoding} encoding.")
         return (None)
 
-@tool
+
 def list_files_in_dir(folder_path: str = ".") -> List[str]:
     """
         Lists all files in the specified folder (non-recursive).
@@ -138,7 +140,7 @@ def list_files_in_dir(folder_path: str = ".") -> List[str]:
 
     return (file_list)
 
-@tool
+
 def agent_modify_line(file_path: str, line_number: int, new_content: str) -> Optional[str]:
     """
         Modifies a specific line in a file based on a 1-indexed line number.
@@ -178,61 +180,126 @@ def agent_modify_line(file_path: str, line_number: int, new_content: str) -> Opt
 
 
 class Reflection(BaseModel):
-    effects: str = Field(description="What are the possible effects of executing the tool calls in this instructions")
-    conflicts: str = Field(description="What are the possible conflicts that may arise from executing the tool calls in this instructions")
-    needed: str = Field(description="What other tool calls might still be needed to achieve the final goal")
-    unneeded: str = Field(description="What tool calls might not be needed to achieve the final goal")
+    effects: str = Field(description="What are the possible effects of executing the function calls in this instructions")
+    conflicts: str = Field(description="What are the possible conflicts that may arise from executing the function calls in this instructions")
+    needed: str = Field(description="What other function calls might still be needed to achieve the final goal")
+    unneeded: str = Field(description="What function calls might not be needed to achieve the final goal")
 
 class Instructions(BaseModel):
-    objective: str = Field(description="How the tool calls in this instructions help achieve the final goal")
-    reflection: Reflection = Field(description="Self-critique of this instructions")
-    tool_calls: List[str] = Field(description="Sequence of tool calls to be executed towards the realization of the final goal")
+    explanation: str = Field(description="How the function calls in this instructions help achieve the final goal")
+    reflection: Reflection = Field(description="Self-critique of the sequence of functions")
+    function_calls: List[Dict[str, str]] = Field(description="Sequence of function calls (dictionary with keys `func_name` and `args`) to be executed towards the realization of the final goal")
 
 
-def execute_tools(state: List[BaseMessage], tools_map: Dict[str, Any]) -> List[BaseMessage]: 
+def execute_tools(state: List[BaseMessage]) -> List[BaseMessage]: 
     tool_messages = []
-    for tool_call in state[-1].tool_calls:
-        if tool_call["name"] =="Instructions":
+    last_message = state[-1]
+
+    print(last_message)
+    print("===============================================================\n")
+
+    tools_map = {
+        "get_modified_files": get_modified_files,
+        "get_file_changes": get_file_changes,
+        "get_file_content": get_file_content,
+        "list_files_in_dir": list_files_in_dir,
+        "agent_modify_line": agent_modify_line
+    }
+
+    for tool_call in last_message.tool_calls:
+        if tool_call["name"] == "Instructions":
             instructions_tool_calls = tool_call["args"].get("instructions_tool_calls", [])
-            tool_results = {}
+
             for instructions_tool_call in instructions_tool_calls:
-                result = tools_map[instructions_tool_call["name"]].invoke(instructions_tool_call["args"])
-                tool_results[instructions_tool_call["id"]] = result
-            tool_messages.append(ToolMessage(content=json.dumps(tool_results), tool_call_id=tool_call["id"]))
+                print(instructions_tool_call)
+                # Execute the sub-tool
+                result = tools_map[instructions_tool_call["fuction_name"]].invoke(instructions_tool_call["args"])
+
+            tool_messages.append(
+                ToolMessage(content=str(result), tool_call_id=tool_call["id"])
+            )
+        else:
+            # Handle normal tool calls if they happen outside of "Instructions"
+            result = tools_map[tool_call["name"]].invoke(tool_call["args"])
+            tool_messages.append(ToolMessage(content=str(result), tool_call_id=tool_call["id"]))
+            
     return (tool_messages)
 
 
 def event_loop(state: List[BaseMessage]) -> str:
     count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
-    if count_tool_visits >= 2:
-        return (END)
-    else:
-        return ("execute_tools")
+    num_iterations = count_tool_visits
+    print(f"---------------------> {num_iterations}")
+    if num_iterations >= 5:
+        return END
+    return "execute_tools"
 
 
 
 if __name__ == "__main__":
 
     # LLMs definition
-    llm_responder_langchain_workflow = ChatGoogleGenerativeAI(model="gemini-3.5-flash", google_api_key="AQ.Ab8RN6II3pE9opZ-ENdyg1dm8GB4XoOv0pU-dMTYd9eNMSCokA")
-    llm_revisor_langchain_workflow = ChatGoogleGenerativeAI(model="gemini-3.5-flash", google_api_key="AQ.Ab8RN6KUrq59ty82WP5wvxPe2jPbHxoDmXzxph_F8E_qoFcTpg")
+    import os
 
-    # Tools definition
-    tools = [get_modified_files, get_file_changes, get_file_content, list_files_in_dir, agent_modify_line]
-    tools_map = { tool.name:tool for tool in tools }
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    gcp_api_key = os.getenv("GCP_API_KEY")
+
+    os.environ["GROQ_API_KEY"] = groq_api_key  
+
+    llm_responder_langchain_workflow = ChatGroq(model="llama-3.1-8b-instant", temperature=0.6)
+    # ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=gcp_api_key)
+    llm_revisor_langchain_workflow = ChatGroq(model="llama-3.1-8b-instant", temperature=0.6)
+    # ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=gcp_api_key)
 
     # Prompt template definition
     prompt_template = ChatPromptTemplate.from_messages([
-        ("system", "{agent_persona}"),
-        MessagesPlaceholder(variable_name="messages")
+        (
+            "system",
+            """
+            You are a Reflexion Agent that can reason about the changes to be made in a GitHub repository.
+
+            You must propose a plan by calling the 'Instructions' tool provided to you. Inside this tool, you can schedule an ordered sequence of operations using the following supported function names and arguments:
+                
+                get_modified_files
+                    Args: None
+                    Returns: List[str]
+
+                get_file_changes
+                    Args: modified_files (List[str])
+                    Returns: str
+
+                get_file_content
+                    Args: file_path (str), encoding (str)
+                    Returns: str
+
+                list_files_in_dir
+                    Args: folder_path (str)
+                    Returns: List[str]
+
+                agent_modify_line
+                    Args: file_path (str), line_number (int), new_content (str)
+                    Returns: Optional[str]
+
+            Follow these steps:
+                1. {instruction}
+                2. Think how the operations above can help achieve the final goal.
+                3. Reflect about the possible consequences of executing each operation.
+                4. Populate the 'Instructions' tool with the exact sequence required.
+            """
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "You must respond by executing the 'Instructions' structured tool mapping out your next steps."
+        ),
     ])
 
     # Nodes logic definition
-    responder_persona = "You are a Responder Agent that can reason about the changes to be made in a GitHub repository. You have access to the following tools: get_modified_files, get_file_changes, get_file_content, list_files_in_dir, and agent_modify_line. Use these tools to gather information and propose changes."
-    responder_langchain_workflow = prompt_template.partial(agent_persona = responder_persona) | llm_responder_langchain_workflow.bind_tools(tools + [Instructions])
+    responder_instruction = ""
+    responder_langchain_workflow = prompt_template.partial(instruction = responder_instruction) | llm_responder_langchain_workflow.bind_tools(tools=[Instructions])
 
-    revisor_persona = "You are a Reflection Agent that can reason about the changes to be made in a GitHub repository. You have access to the following tools: get_modified_files, get_file_changes, get_file_content, list_files_in_dir, and agent_modify_line. Use these tools to gather information and propose changes."
-    revisor_langchain_workflow = prompt_template.partial(agent_persona = revisor_persona) | llm_revisor_langchain_workflow.bind_tools(tools + [Instructions])
+    revisor_instruction = ""
+    revisor_langchain_workflow = prompt_template.partial(instruction = revisor_instruction) | llm_revisor_langchain_workflow.bind_tools(tools=[Instructions])
 
     # Graph definition
     graph = MessageGraph()
@@ -250,8 +317,9 @@ if __name__ == "__main__":
     # Execute the agent
     app = graph.compile()
     responses = app.invoke(
-        """I'm pre-diabetic and need to lower my blood sugar, and I have heart issues.
-        What breakfast foods should I eat and avoid"""
+        """
+            Add a `return(0)` statement at the bottom of the main function of the file test.py, without modifying the rest of the code.
+        """
     )
 
     # Visualize the messages history
---
+++ test.py
@@ -0,0 +1,4 @@
+def main():
+    print("Hello world!")
+
+    # Insert return statement here
